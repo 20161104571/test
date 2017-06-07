93c0349acfe0e3e6b49bf56e30322b5d9595e9fd
20161104571 <852169778@qq.com>
2017-06-07 10:21:32 +0800

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by w20161104571 on 17/6/7.
+//  Copyright © 2017年 w20161104571. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
