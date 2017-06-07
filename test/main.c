1cb9be944d5e233d06e2b420f3438741d81bb67d
20161104571 <852169778@qq.com>
2017-06-07 11:31:47 +0800

    Sort
    second

--- test/main.c
+++ test/main.c
@@ -8,8 +8,33 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    int a[10];
+    int i,j,k;
+    for(i+0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<9;i++)
+    {
+        for(j=0;j<9-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                k=a[j];
+                a[j]=a[j+1];
+                a[j+1]=k;
+            }
+        }
+    }
+    for(i=0;i<10;i++)
+    {
+        printf("%d ",a[i]);
+    }
     return 0;
 }
+    
+        
+    
+   
